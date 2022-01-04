619f6edd3641263e22415e83b472442bc137d38c
F. W <fw@loki.valhalla.lan>
2022-01-04 09:48:24 +0100

    added treesitter config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,6 +45,12 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- Treesitter
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
   -- Theming
   use "arcticicestudio/nord-vim"
 
