b5e7353c36dc3e91f2ecbdc458a6e1394b7e3077
picture-vision <fw@picture-vision.com>
2022-04-16 07:55:42 +0200

    cleanup

--- lua/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd[[colorscheme nord]]
--- Example config in lua
-vim.g.nord_contrast = false
-vim.g.nord_borders = false
-vim.g.nord_disable_background = false
-vim.g.nord_italic = true
-
--- Load the colorscheme
-require('nord').set()
