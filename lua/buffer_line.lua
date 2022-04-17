894e87cba52de6ff9b13c8381e0cf9f88d3ec333
picture-vision <fw@picture-vision.com>
2022-04-17 11:13:44 +0200

    bufferline modifications

--- lua/buffer_line.lua
+++ lua/buffer_line.lua
@@ -14,8 +14,8 @@ bufferline.setup {
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
     indicator_icon = "▎",
-    buffer_close_icon = "",
-    -- buffer_close_icon = '',
+    -- buffer_close_icon = "",
+    buffer_close_icon = '',
     modified_icon = "●",
     close_icon = "",
     -- close_icon = '',
@@ -57,9 +57,9 @@ bufferline.setup {
     -- end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = false,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
